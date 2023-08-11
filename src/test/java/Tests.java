f9cca61e11a82f65b580923a467557d3b40a8400
Kononirin <kononirin@gmail.com>
2023-08-11 17:01:33 +0500

    add testextox

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,13 +5,35 @@ import org.testng.annotations.Test;
 
 public class Tests extends BaseTest{
 
-    @Test
-    public void testGetHeaderUrl() {
+    private static final By ELEMENTS = By.xpath("//div[1][@class='card mt-4 top-card']");
 
-        driver.findElement(By.xpath("//div[1][@class='card mt-4 top-card']")).click();
+    @Test
+    public void testGetHeader() {
+        driver.findElement(ELEMENTS).click();
 
         WebElement mainHeader = driver.findElement(By.xpath("//div[@class='main-header']"));
 
         Assert.assertEquals(mainHeader.getText(), "Elements");
+    }
+
+    @Test
+    public void testTextBox() {
+        driver.findElement(ELEMENTS).click();
+        driver.findElement(By.id("item-0")).click();
+        driver.findElement(By.id("userName")).sendKeys("Irina Shlyapina");
+        driver.findElement(By.id("userEmail")).sendKeys("kononirin@yandex.ru");
+        driver.findElement(By.id("currentAddress")).sendKeys("Perm, Turchevicha 6-320");
+        driver.findElement(By.id("permanentAddress")).sendKeys("Perm, Turchevicha 6-320");
+        driver.findElement(By.id("submit")).click();
+
+        WebElement name = driver.findElement(By.xpath("//p[text()='Irina Shlyapina']"));
+        WebElement email = driver.findElement(By.xpath("//p[@id='email']"));
+        WebElement currentAddress = driver.findElement(By.xpath("//p[@id='currentAddress']"));
+        WebElement permanentAddress = driver.findElement(By.xpath("//p[@id='permanentAddress']"));
+
+        Assert.assertEquals(name.getText(), "Name:Irina Shlyapina");
+        Assert.assertEquals(email.getText(), "Email:kononirin@yandex.ru");
+        Assert.assertEquals(currentAddress.getText(), "Current Address :Perm, Turchevicha 6-320");
+        Assert.assertEquals(permanentAddress.getText(), "Permananet Address :Perm, Turchevicha 6-320");
     }
 }
